f4687d79dcbc47b4b3d0f6a33108bbff9aea00bf
Tiago Y. Fujii <tiagoyukio12@gmail.com>
2019-11-13 20:43:59 -0300

    Add canvas layer for HUD
    HUD and pause menu weren't showing without CanvasLayer.

--- scripts/Pause.gd
+++ scripts/Pause.gd
@@ -6,28 +6,15 @@ func _input(event):
 
 func changePauseState():
 	var pause_state = not get_tree().paused
+	print(pause_state)
 	get_tree().paused = pause_state
 	visible = pause_state
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
-# func _ready():
-# 	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 
 func _on_ResumeButton_pressed():
 	changePauseState()
 
 func _on_OptionsButton_pressed():
 	pass # Replace with function body.
-
 
 func _on_QuitButton_pressed():
 	changePauseState()
